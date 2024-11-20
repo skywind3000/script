3e16af245c7b3bf4d0f1bf7cabc61ba7367b64df
skywind3000 <skywind3000@163.com>
2024-11-21 03:25:57 +0800

    update init.sh

---
+++ system/init.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#======================================================================
+#
+# init.sh - script initialize
+#
+# Created by skywind on 2024/11/21
+# Last Modified: 2024/11/21 03:16:20
+#
+#======================================================================
+
+# change directory to script home
+cd "$(dirname $0)"
+INIT_HOME="$(pwd)"
+
+# eval each file inside "init" folder
+if [ -d "$INIT_HOME/init" ]; then
+	for f in $INIT_HOME/init/*; do
+		cd "$INIT_HOME/init"
+		[ -e "$f" ] && . "$f"
+	done
+fi
+
+# execute each file inside "run" folder
+if [ -d "$INIT_HOME/run" ]; then
+	for f in $INIT_HOME/run/*; do
+		cd "$INIT_HOME/run"
+		[ -x "$f" ] && "$f"
+	done
+fi
+
+
