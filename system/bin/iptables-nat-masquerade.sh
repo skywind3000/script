9455d6aeab7863242f50da43837decc68f80b512
skywind3000 <skywind3000@163.com>
2024-11-21 10:42:24 +0800

    update iptables

--- system/bin/iptables-nat-masquerade.sh
+++ system/bin/iptables-nat-masquerade.sh
@@ -1,10 +1,24 @@
 #! /bin
 network="$1"
+remove="$2"
+mode="-A"
 
 if [ -z "$network" ]; then
 	echo "usage: $0 {network}"
 	exit 1
 fi
 
-iptables -t nat -A POSTROUTING -s $network ! -d $network -j MASQUERADE
+if [ "$remove" != "-" ]; then
+	mode="-A"
+	echo "append"
+else
+	mode="-D"
+	echo "remove"
+fi
+
+if [ "$network" != "true" ]; then
+	iptables -t nat $mode POSTROUTING -s $network ! -d $network -j MASQUERADE
+else
+	iptables -t nat $mode POSTROUTING -j MASQUERADE
+fi
 
