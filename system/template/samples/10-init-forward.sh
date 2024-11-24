ca2893ec81c779c123c4562afbb2e73961ae5703
skywind3000 <skywind3000@163.com>
2024-11-24 22:52:53 +0800

    Add IPv6 forwarding configuration and adjust network tuning settings in 10-init-forward.sh script. Also, disable redirect and adjust packet filtering settings. Update NAT masquerade rule in the script.

--- system/template/samples/10-init-forward.sh
+++ system/template/samples/10-init-forward.sh
@@ -8,7 +8,14 @@
 #======================================================================
 
 # enable forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
+# tune
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.all.rp_filter=0
+sysctl -w net.ipv4.conf.lo.rp_filter=0
 
 # nat masquerade
 iptables -t nat -A POSTROUTING -j MASQUERADE
