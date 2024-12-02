a998abca3b5b5c0f39cf666198e8f3344014ac70
skywind3000 <skywind3000@163.com>
2024-12-02 22:04:25 +0800

    Add new scripts for banning/unbanning IPs and sets in iptables
    - Added iptables-ip-ban.sh for banning specific IP addresses
    - Added iptables-ip-unban.sh for unbanning specific IP addresses
    - Added iptables-set-ban.sh for banning IPs from ipset
    - Renamed iptables-mark-set.sh to iptables-set-mark.sh
    - Added iptables-set-unban.sh for unbanning IPs from ipset

---
+++ system/bin/iptables-set-ban.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#======================================================================
+#
+# iptables-set-ban.sh - ban IPs from ipset
+#
+# Created by skywind on 2024/12/02
+# Last Modified: 2024/12/02 22:00:10
+#
+#======================================================================
+SETNAME="$1"
+
+if [ -z "$SETNAME" ]; then
+	echo "usage: $0 {SETNAME}"
+	exit 1
+fi
+
+iptables -I INPUT -m set --match-set "$SETNAME" src -j DROP
+
---
+++ system/bin/iptables-set-unban.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#======================================================================
+#
+# iptables-set-unban.sh - unban IPs from ipset
+#
+# Created by skywind on 2024/12/02
+# Last Modified: 2024/12/02 22:00:10
+#
+#======================================================================
+SETNAME="$1"
+
+if [ -z "$SETNAME" ]; then
+	echo "usage: $0 {SETNAME}"
+	exit 1
+fi
+
+iptables -D INPUT -m set --match-set "$SETNAME" src -j DROP
+
+
