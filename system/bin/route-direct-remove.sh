fe003638deda740bfc3c307cfc9495681bcec2c7
skywind3000 <skywind3000@163.com>
2024-12-10 15:11:18 +0800

    update file

--- system/bin/route-direct-remove.sh
+++ system/bin/route-direct-remove.sh
@@ -12,9 +12,10 @@ GATEWAY="$2"
 VPN_SERVER="$3"
 VPN_DEVICE="$4"
 VPN_GATEWAY="$5"
+VPN_SUBNET="$6"
 
 if [ -z "$VPN_GATEWAY" ]; then
-	echo "usage: $0 {dev} {gw} {vpn_server} {vpn_dev} {vpn_gw}"
+	echo "usage: $0 {dev} {gw} {vpn_server} {vpn_dev} {vpn_gw} [vpn_subnet]"
 	echo "example:"
 	echo "$0 eth0 192.168.1.1 202.115.8.13 tun0 10.8.2.1 10.8.2.0/24"
 	exit 1
@@ -27,6 +28,11 @@ ip rule del prio 100 lookup 200
 
 # direct link use default gateway
 ip rule del prio 101 fwmark 700 lookup main
+
+# vpn sub network
+if [ -n "$VPN_SUBNET" ]; then
+	ip route del table 201 $VPN_SUBNET dev $VPN_DEVICE
+fi
 
 # passwall link use vpn gateway
 ip route del table 201 default via $VPN_GATEWAY dev $VPN_DEVICE
