254e554f97a9a9d2136c2e18e972b220db4662b1
skywind3000 <skywind3000@163.com>
2024-12-10 15:13:06 +0800

    update

--- system/template/samples/device-openvpn-direct.sh
+++ system/template/samples/device-openvpn-direct.sh
@@ -3,25 +3,22 @@
 # echo "[device] $IFACE $1" | /etc/script/bin/append-log.sh /tmp/record.log
 
 DEV="eth0"
-GW="192.168.1.1"
-VPN_SERVER="108.61.217.246"
-VPN_DEV="tun1"
-VPN_GW="10.8.108.24"
+GW="10.66.5.1"
+VPN_SERVER="111.229.152.96"
+VPN_DEV="tun2"
+VPN_GW="10.8.8.1"
+VPN_SUBNET="10.8.8.0/24"
 
 if [ "$1" = "up" ]; then
 	# install route rules for vpn
-	/etc/script/bin/route-direct-install.sh $DEV $GW $VPN_SERVER $VPN_DEV $VPN_GW
+	/etc/script/bin/route-direct-install.sh $DEV $GW $VPN_SERVER $VPN_DEV $VPN_GW $VPN_SUBNET
 
 	# change mtu
 	# ip link set $VPN_DEV mtu 1400
 
 elif [ "$1" = "postdown" ]; then
 	# remove route rules for vpn
-	/etc/script/bin/route-direct-remove.sh $DEV $GW $VPN_SERVER $VPN_DEV $VPN_GW
-
-	# china ipset
-	ip rule del prio 100 fwmark 666 lookup 666
-	ip route del table 666 default via $GW dev $DEV
+	/etc/script/bin/route-direct-remove.sh $DEV $GW $VPN_SERVER $VPN_DEV $VPN_GW $VPN_SUBNET
 fi
 
 
