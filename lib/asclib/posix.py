adf5254eedae4e8766847b680fa6df71fa9d2cc2
skywind3000 <skywind3000@163.com>
2024-12-24 15:18:07 +0800

    Refactor posix.py: Added functions to ensure path exists, get standard path, check if file is in directory, find files recursively.

--- lib/asclib/posix.py
+++ lib/asclib/posix.py
@@ -15,7 +15,7 @@ import codecs
 
 try:
     from . import core
-except:
+except ImportError:
     import core
 
 UNIX = core.UNIX
@@ -85,112 +85,59 @@ def which(name, prefix = None, postfix = None):
 
 
 #----------------------------------------------------------------------
-# load content
+# ensure path exists
 #----------------------------------------------------------------------
-def load_file_content(filename, mode = 'r'):
-    if hasattr(filename, 'read'):
-        try: content = filename.read()
-        except: content = None
-        return content
-    try:
-        if '~' in filename:
-            filename = os.path.expanduser(filename)
-        fp = open(filename, mode)
-        content = fp.read()
-        fp.close()
-    except:
-        content = None
-    return content
-
-
-#----------------------------------------------------------------------
-# save file content
-#----------------------------------------------------------------------
-def save_file_content(filename, content, mode = 'w'):
-    try:
-        fp = open(filename, mode)
-        fp.write(content)
-        fp.close()
-    except:
+def ensure_path(path):
+    if not path:
         return False
-    return True
-
-
-#----------------------------------------------------------------------
-# auto detect encoding and decode into a string
-#----------------------------------------------------------------------
-def string_auto_decode(payload, encoding = None):
-    content = None
-    if payload is None:
-        return None
-    if hasattr(payload, 'read'):
-        try: content = payload.read()
-        except: pass
-    else:
-        content = payload
-    if sys.version_info[0] >= 3:
-        if isinstance(content, str):
-            return content
-    else:
-        # pylint: disable-next=else-if-used
-        if isinstance(content, unicode):   # noqa
-            return content
-    if content is None:
-        return None
-    if not isinstance(payload, bytes):
-        return str(payload)
-    if content[:3] == b'\xef\xbb\xbf':
-        return content[3:].decode('utf-8', 'ignore')
-    elif encoding is not None:
-        return content.decode(encoding, 'ignore')
-    guess = [sys.getdefaultencoding(), 'utf-8']
-    if sys.stdout and sys.stdout.encoding:
-        guess.append(sys.stdout.encoding)
-    try:
-        import locale
-        guess.append(locale.getpreferredencoding())
-    except:
-        pass
-    visit = {}
-    text = None
-    for name in guess + ['gbk', 'ascii', 'latin1']:
-        if name in visit:
-            continue
-        visit[name] = 1
+    if not os.path.exists(path):
         try:
-            text = content.decode(name)
-            break
+            os.makedirs(path)
         except:
-            pass
-    if text is None:
-        text = content.decode('utf-8', 'ignore')
-    return text
-
-
-
-#----------------------------------------------------------------------
-# load file and guess encoding
-#----------------------------------------------------------------------
-def load_file_text(filename, encoding = None):
-    content = load_file_content(filename, 'rb')
-    if content is None:
-        return None
-    return string_auto_decode(content, encoding)
-
-
-#----------------------------------------------------------------------
-# save file text
-#----------------------------------------------------------------------
-def save_file_text(filename, content, encoding = None):
-    if encoding is None:
-        encoding = 'utf-8'
-    if (not isinstance(content, unicode)) and isinstance(content, bytes):
-        return save_file_content(filename, content)
-    with codecs.open(filename, 'w', 
-            encoding = encoding, 
-            errors = 'ignore') as fp:
-        fp.write(content)
+            return False
     return True
+
+
+#----------------------------------------------------------------------
+# get standard path 
+#----------------------------------------------------------------------
+def __stdpath(name):
+    if not name:
+        name = 'temp'
+    if name == 'temp':
+        if UNIX:
+            return os.path.normpath('/tmp')
+        else:
+            t = os.environ.get('TEMP', 'C:/Temp')
+            return os.path.normpath(t)
+    if name in ('home', 'user', '~'):
+        return os.path.expanduser('~')
+    if name == 'config':
+        if 'XDG_CONFIG_HOME' in os.environ:
+            return os.path.normpath(os.environ['XDG_CONFIG_HOME'])
+        return os.path.normpath(os.path.expanduser('~/.config'))
+    if name == 'data':
+        if 'XDG_DATA_HOME' in os.environ:
+            return os.path.normpath(os.environ['XDG_DATA_HOME'])
+        return os.path.normpath(os.path.expanduser('~/.local/share'))
+    if name == 'cache':
+        if 'XDG_CACHE_HOME' in os.environ:
+            return os.path.normpath(os.environ['XDG_CACHE_HOME'])
+        return os.path.normpath(os.path.expanduser('~/.cache'))
+    if name == 'desktop':
+        if 'XDG_DESKTOP_DIR' in os.environ:
+            return os.path.normpath(os.environ['XDG_DESKTOP_DIR'])
+        return os.path.normpath(os.path.expanduser('~/Desktop'))
+    return None
+
+
+#----------------------------------------------------------------------
+# get standard path
+#----------------------------------------------------------------------
+def stdpath(name):
+    path = __stdpath(name)
+    ensure_path(path)
+    return path
 
 
 #----------------------------------------------------------------------
@@ -250,13 +197,206 @@ def find_files(root, pattern = '*', recursive = True, mode = 0):
 
 
 #----------------------------------------------------------------------
+# auto detect encoding and decode into a string
+#----------------------------------------------------------------------
+def string_auto_decode(payload, encoding = None):
+    content = None
+    if payload is None:
+        return None
+    if hasattr(payload, 'read'):
+        try: content = payload.read()
+        except: pass
+    else:
+        content = payload
+    if sys.version_info[0] >= 3:
+        if isinstance(content, str):
+            return content
+    else:
+        # pylint: disable-next=else-if-used
+        if isinstance(content, unicode):   # noqa
+            return content
+    if content is None:
+        return None
+    if not isinstance(payload, bytes):
+        return str(payload)
+    if content[:3] == b'\xef\xbb\xbf':
+        return content[3:].decode('utf-8', 'ignore')
+    elif encoding is not None:
+        return content.decode(encoding, 'ignore')
+    guess = [sys.getdefaultencoding(), 'utf-8']
+    if sys.stdout and sys.stdout.encoding:
+        guess.append(sys.stdout.encoding)
+    try:
+        import locale
+        guess.append(locale.getpreferredencoding())
+    except:
+        pass
+    visit = {}
+    text = None
+    for name in guess + ['gbk', 'ascii', 'latin1']:
+        if name in visit:
+            continue
+        visit[name] = 1
+        try:
+            text = content.decode(name)
+            break
+        except:
+            pass
+    if text is None:
+        text = content.decode('utf-8', 'ignore')
+    return text
+
+
+#----------------------------------------------------------------------
+# replace file atomicly
+#----------------------------------------------------------------------
+def replace_file(srcname, dstname):
+    if sys.platform[:3] != 'win':
+        try:
+            os.rename(srcname, dstname)
+        except OSError:
+            return False
+    else:
+        import ctypes.wintypes
+        kernel32 = ctypes.windll.kernel32
+        wp, vp, cp = ctypes.c_wchar_p, ctypes.c_void_p, ctypes.c_char_p
+        DWORD, BOOL = ctypes.wintypes.DWORD, ctypes.wintypes.BOOL
+        kernel32.ReplaceFileA.argtypes = [ cp, cp, cp, DWORD, vp, vp ]
+        kernel32.ReplaceFileW.argtypes = [ wp, wp, wp, DWORD, vp, vp ]
+        kernel32.ReplaceFileA.restype = BOOL
+        kernel32.ReplaceFileW.restype = BOOL
+        kernel32.GetLastError.argtypes = []
+        kernel32.GetLastError.restype = DWORD
+        success = False
+        try:
+            os.rename(srcname, dstname)
+            return True
+        except OSError:
+            pass
+        if sys.version_info[0] < 3 and isinstance(srcname, str):
+            hr = kernel32.ReplaceFileA(dstname, srcname, None, 2, None, None)
+        else:
+            hr = kernel32.ReplaceFileW(dstname, srcname, None, 2, None, None)
+        if not hr:
+            return False
+    return True
+
+
+#----------------------------------------------------------------------
+# random temp 
+#----------------------------------------------------------------------
+def tmpname(filename, fill = 5):
+    import time, os, random
+    while 1:
+        name = '.' + str(int(time.time() * 1000000))
+        for i in range(fill):
+            k = random.randint(0, 51)
+            name += (k < 26) and chr(ord('A') + k) or chr(ord('a') + k - 26)
+        test = filename + name + str(os.getpid())
+        if not os.path.exists(test):
+            return test
+    return None
+
+
+#----------------------------------------------------------------------
+# load content
+#----------------------------------------------------------------------
+def load_file_content(filename, mode = 'r'):
+    if hasattr(filename, 'read'):
+        try: content = filename.read()
+        except: content = None
+        return content
+    try:
+        if '~' in filename:
+            filename = os.path.expanduser(filename)
+        fp = open(filename, mode)
+        content = fp.read()
+        fp.close()
+    except:
+        content = None
+    return content
+
+
+#----------------------------------------------------------------------
+# save file content
+#----------------------------------------------------------------------
+def save_file_content(filename, content, mode = 'w'):
+    try:
+        fp = open(filename, mode)
+        fp.write(content)
+        fp.close()
+    except:
+        return False
+    return True
+
+
+#----------------------------------------------------------------------
+# save file atomicly
+#----------------------------------------------------------------------
+def save_file_content_atomic(filename, content, mode = 'w'):
+    tmp = tmpname(filename)
+    if not tmp:
+        return False
+    if not save_file_content(tmp, content, mode):
+        return False
+    if not replace_file(tmp, filename):
+        return False
+    return True
+
+
+#----------------------------------------------------------------------
+# load file and guess encoding
+#----------------------------------------------------------------------
+def load_file_text(filename, encoding = None):
+    content = load_file_content(filename, 'rb')
+    if content is None:
+        return None
+    return string_auto_decode(content, encoding)
+
+
+#----------------------------------------------------------------------
+# save file text
+#----------------------------------------------------------------------
+def save_file_text(filename, content, encoding = None):
+    if encoding is None:
+        encoding = 'utf-8'
+    if isinstance(content, bytes):
+        return save_file_content(filename, content)
+    with codecs.open(filename, 'w', 
+            encoding = encoding, 
+            errors = 'ignore') as fp:
+        fp.write(content)
+    return True
+
+
+#----------------------------------------------------------------------
+# save file text atomicly
+#----------------------------------------------------------------------
+def save_file_text_atomic(filename, content, encoding = None):
+    if encoding is None:
+        encoding = 'utf-8'
+    if isinstance(content, bytes):
+        return save_file_content_atomic(filename, content)
+    tmp = tmpname(filename)
+    if not tmp:
+        return False
+    with codecs.open(tmp, 'w', 
+            encoding = encoding, 
+            errors = 'ignore') as fp:
+        fp.write(content)
+    if not replace_file(tmp, filename):
+        return False
+    return True
+
+
+#----------------------------------------------------------------------
 # testing suit
 #----------------------------------------------------------------------
 if __name__ == '__main__':
     def test1():
         content = load_file_content(__file__)
         print(content)
-        print(which('gcc'))
+        print(which('gcc2'))
         return 0
     test1()
 
---
+++ lib/asclib/state.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#======================================================================
+#
+# state.py - 
+#
+# Created by skywind on 2024/12/24
+# Last Modified: 2024/12/24 14:26:45
+#
+#======================================================================
+from __future__ import print_function, unicode_literals
+import sys
+import os
+import json
+import pickle
+
+try:
+    from . import core
+    from . import posix
+except ImportError:
+    import core
+    import posix
+
+UNIX = core.UNIX
+
+
+#----------------------------------------------------------------------
+# ini
+#----------------------------------------------------------------------
+def load_ini(filename, encoding = None):
+    text = posix.load_file_text(filename, encoding)
+    config = {}
+    sect = 'default'
+    if text is None:
+        return None
+    for line in text.split('\n'):
+        line = line.strip('\r\n\t ')
+        # pylint: disable-next=no-else-continue
+        if not line:   # noqa
+            continue
+        elif line[:1] in ('#', ';'):
+            continue
+        elif line.startswith('['):
+            if line.endswith(']'):
+                sect = line[1:-1].strip('\r\n\t ')
+                if sect not in config:
+                    config[sect] = {}
+        else:
+            pos = line.find('=')
+            if pos >= 0:
+                key = line[:pos].rstrip('\r\n\t ')
+                val = line[pos + 1:].lstrip('\r\n\t ')
+                if sect not in config:
+                    config[sect] = {}
+                config[sect][key] = val
+    return config
+
+
+#----------------------------------------------------------------------
+# save ini 
+#----------------------------------------------------------------------
+def save_ini(filename, config, atomic = False):
+    output = []
+    sectnames = ['default']
+    for sect in config:
+        if sect != 'default':
+            sectnames.append(sect)
+    for sectname in sectnames:
+        if sectname not in config:
+            continue
+        sectname = sectname.strip('\r\n\t ')
+        output.append('[%s]' % sectname)
+        for key in config[sectname]:
+            val = config[sectname][key]
+            output.append('%s=%s' % (key, val))
+        output.append('')
+    text = '\n'.join(output)
+    if atomic:
+        if not posix.save_file_text_atomic(filename, text):
+            return False
+    if not posix.save_file_text(filename, text):
+        return False
+    return True
+
+
+#----------------------------------------------------------------------
+# json load
+#----------------------------------------------------------------------
+def load_json(filename, encoding = None):
+    try:
+        text = posix.load_file_text(filename, encoding)
+        if text is None:
+            return None
+        return json.loads(text)
+    except:
+        return None
+    return None
+
+
+#----------------------------------------------------------------------
+# json save 
+#----------------------------------------------------------------------
+def save_json(filename, obj, atomic = False):
+    text = json.dumps(obj, indent = 4) + '\n'
+    if not atomic:
+        if not posix.save_file_text(filename, text):
+            return False
+    else:
+        if not posix.save_file_text_atomic(filename, text):
+            return False
+    return True
+
+
+#----------------------------------------------------------------------
+# pickle load
+#----------------------------------------------------------------------
+def load_pickle(filename):
+    with open(filename, 'rb') as fp:
+        return pickle.load(fp)
+    return None
+
+
+#----------------------------------------------------------------------
+# pickle save
+#----------------------------------------------------------------------
+def save_pickle(filename, obj, atomic = False):
+    data = pickle.dumps(obj)
+    if atomic:
+        return posix.save_file_content_atomic(filename, data)
+    return posix.save_file_content(filename, data)
+
+
