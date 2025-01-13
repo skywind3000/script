42e61f606ae013fb9be3224766fcc693c8872c5a
skywind3000 <skywind3000@163.com>
2025-01-13 14:59:08 +0800

    add a script to rsync remote files to /home/data/sync

---
+++ system/bin/rsync-remote-mm.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+if [ ! -d /home/data/sync ]; then
+	echo "/home/data/sync doesn't exist"
+	exit 1
+elif [ ! -x "$(which rsync)" ]; then
+	echo "rsync is not installed"
+	exit 1
+fi
+
+rsync -avrz --delete -e 'ssh -p32200' bbtest.skywind.me:/home/data/sync/ /home/data/sync/
+
+exit 0
+
